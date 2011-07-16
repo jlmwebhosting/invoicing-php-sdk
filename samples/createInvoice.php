4c70d1d1d260d0606d2e034d9d5995435a2d6756
Sidney Maestre <sid@crushitmobile.com>
2011-07-16 10:34:20 -0700

    Changed the $path from ./lib to ../lib

--- samples/createInvoice.php
+++ samples/createInvoice.php
@@ -1,6 +1,6 @@
 <?php
 
-$path = './lib';
+$path = '../lib';
 set_include_path(get_include_path() . PATH_SEPARATOR . $path);
 require_once('services/Invoice/InvoiceService.php');
 require_once('PPLoggingManager.php');
